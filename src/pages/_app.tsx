3462c6ec3f8381dadc1ccf3ea9b6a615c7a2b7f4
Tomasz Rogala <tomasz.rogala1505@gmail.com>
2022-04-11 18:28:21 +0200

    Initial Commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+
+import createEmotionCache from '../createEmotionCache';
+import lightThemeOptions from '../theme';
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+const lightTheme = createTheme(lightThemeOptions);
+
+const MyApp: React.FunctionComponent<MyAppProps> = props => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
